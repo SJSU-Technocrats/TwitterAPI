a868bc7a01cb69c1178c1bebf62efdf4058225bc
geethu <geethu@192.168.1.10>
2019-10-20 08:54:23 -0700

    Test case for Twitter Service and Negative cases

--- src/test/java/edu/sjsu/moni/services/TwitterServiceTest.java
+++ src/test/java/edu/sjsu/moni/services/TwitterServiceTest.java
@@ -1,19 +1,13 @@
-package edu.sjsu.moni.controllers;
+package edu.sjsu.moni.services;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Random;
 
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -22,77 +16,71 @@ import edu.sjsu.moni.models.TweetResponse;
 import twitter4j.TwitterException;
 
 @SpringBootTest
-@RunWith(MockitoJUnitRunner.class)
-@ExtendWith(MockitoExtension.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class TwitterApiControllerTest {
-
-	@Autowired
-	TwitterApiController controller;
-
+class TwitterServiceTest {
+	
+	@Autowired 
+	TwitterService service;
 	@Autowired
 	TweetResponse response;
-
 	@Autowired
 	TweetRequest request;
-
-	@Test
+	
 	@Order(1)
+	@Test
 	void testCreateTweet() throws TwitterException {
 		request.setText("Test Tweet " + new Random().nextInt());
-		response = controller.createTweet(request);
+		response = service.createTweet(request);
 		assertNotNull(response.getId(), "Twitter Id is not null");
+		
 	}
-
-	@Test
+	
 	@Order(2)
-	void testGetTweet() {
+	@Test
+	void testGetTweet() throws TwitterException {
 		String id = response.getId();
-		response = controller.getTweet(id);
+		response = service.getTweet(id);
 		assertTrue(id.equalsIgnoreCase(response.getId()));
 	}
-
-	@Test
+	
 	@Order(3)
-	void testDeleteTweet() throws TwitterException {
-		String deleteTweet = controller.deleteTweet(response.getId());
-		assertTrue("Successfully Deleted!".equalsIgnoreCase(deleteTweet));
-	}
-
 	@Test
+	void testDeleteTweet() throws TwitterException {
+		String id = service.deleteTweet(response.getId());
+		assertTrue(id.equalsIgnoreCase("Successfully Deleted!"));
+	}
+	
 	@Order(4)
+	@Test
 	void testCreateTweetNegative() {
 		request.setText("");
 		try {
-			response = controller.createTweet(request);
+			response = service.createTweet(request);
 		} catch (Exception e) {
 			response = null;
 		}
 		assertNull(response);
-
 	}
-
-	@Test
+	
 	@Order(5)
+	@Test
 	void testGetTweetNegative() {
 		try {
-			response = controller.getTweet(null);
+			response = service.getTweet(null);
 		} catch (Exception e) {
 			response = null;
 		}
 		assertNull(response);
 	}
-
-	@Test
+	
 	@Order(6)
+	@Test
 	void testDeleteTweetNegative() {
 		String delete;
 		try {
-			delete = controller.deleteTweet(null);
+			delete = service.deleteTweet(null);
 		} catch (Exception e) {
 			delete = "Successfully Deleted!";
 		}
 		assertTrue("Successfully Deleted!".equalsIgnoreCase(delete));
 	}
-
 }
